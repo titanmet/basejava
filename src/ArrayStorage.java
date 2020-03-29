6b0661a2ef43e1bf5d34930e24eb3fd1664976c1
titanmet <titanmet@mail.ru>
2020-03-29 21:43:17 +0300

    hw01

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,52 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
+
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = storage[i];
+        }
+        return result;
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
     }
 }
