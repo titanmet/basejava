51b07141e74b4474b47b62b4c17985131c926bdc
titanmet <1973MAgiCC1973>
2020-04-07 21:58:36 +0300

    File

---
+++ src/MainFile.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File(".\\.gitignore");
+        System.out.println(file.getCanonicalPath());
+        File dir = new File(".\\src");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : list) {
+                System.out.println(name);
+            }
+        }
+        
+    }
+}
