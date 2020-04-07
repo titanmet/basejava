5d9bb25a09cb27db045184dc9a1af2bee4f7bc1d
titanmet <titanmet@mail.ru>
2020-04-07 22:22:44 +0300

    File

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,10 +1,17 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainFile {
-    public static void main(String[] args) throws IOException {
-        File file = new File(".\\.gitignore");
-        System.out.println(file.getCanonicalPath());
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
         File dir = new File(".\\src");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
@@ -14,5 +21,10 @@ public class MainFile {
             }
         }
 
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
