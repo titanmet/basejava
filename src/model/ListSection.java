6a9674e2b1b387483f86b402894fcb7b6cebe10f
titanmet <titanmet@mail.ru>
2020-04-10 19:11:14 +0300

    Serializable

--- src/model/ListSection.java
+++ src/model/ListSection.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class ListSection extends Section {
+    private static final long serialVersionUID = 1L;
+
     private final List<String> items;
 
     public ListSection(String... items) {
--- src/model/Organization.java
+++ src/model/Organization.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
@@ -10,7 +11,8 @@ import java.util.Objects;
 import static util.DateUtil.NOW;
 import static util.DateUtil.of;
 
-public class Organization {
+public class Organization implements Serializable {
+    private static final long serialVersionUID = 1L;
     private final Link homePage;
     private List<Position> positions = new ArrayList<>();
 
@@ -43,7 +45,7 @@ public class Organization {
                 "homePage=" + homePage + '}';
     }
 
-    public static class Position {
+    public static class Position implements Serializable {
         private final LocalDate startDate;
         private final LocalDate endDate;
         private final String title;
