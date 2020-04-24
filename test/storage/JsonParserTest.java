aad1c4a4093c2ff5b8394eb906f8adcaa96ee396
titanmet <titanmet@mail.ru>
2020-04-24 10:20:51 +0300

    HW15

---
+++ test/storage/JsonParserTest.java
@@ -0,0 +1,29 @@
+package storage;
+
+import model.Resume;
+import model.Section;
+import model.TextSection;
+import org.junit.Assert;
+import org.junit.Test;
+import util.JsonParser;
+
+import static storage.TestData.R1;
+
+public class JsonParserTest {
+    @Test
+    public void testResume() throws Exception {
+        String json = JsonParser.write(R1);
+        System.out.println(json);
+        Resume resume = JsonParser.read(json, Resume.class);
+        Assert.assertEquals(R1, resume);
+    }
+
+    @Test
+    public void write() throws Exception {
+        Section section1 = new TextSection("Objective1");
+        String json = JsonParser.write(section1, Section.class);
+        System.out.println(json);
+        Section section2 = JsonParser.read(json, Section.class);
+        Assert.assertEquals(section1, section2);
+    }
+}
