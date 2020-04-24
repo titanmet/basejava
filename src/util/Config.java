58c125931ed134a624add18343ea8ce8d3b6cce4
titanmet <titanmet@mail.ru>
2020-04-24 10:28:18 +0300

    HW15

--- src/util/Config.java
+++ src/util/Config.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config\\resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;;
@@ -31,6 +31,15 @@ public class Config {
             throw new IllegalStateException("Invalid config file "
                     + PROPS.getAbsolutePath());
         }
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 
     public File getStorageDir() {
